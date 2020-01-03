e749557faebac5ae5048fd6101f7e4da80fe6c34
mikekucherov <mikekucherov@yandex.ru>
2020-01-03 23:32:07 +0300

    fix prop name

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class QuizQuestionButton extends Component {
   render() {
     return <li>
-      <button>{this.props.buttonText}</button>
+      <button>{this.props.button_text}</button>
     </li>;
   }
 }
